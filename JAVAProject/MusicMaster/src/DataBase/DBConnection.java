dde0504eaa7e2f78b5d9c174639c8a99800917c7
yakir pinchas <yakir.pinchas@live.biu.ac.il>
2019-01-05 21:49:50 +0200

    create dbconnectionconfiguratio.txt and connect to txt in java

--- JAVAProject/MusicMaster/src/DataBase/DBConnection.java
+++ JAVAProject/MusicMaster/src/DataBase/DBConnection.java
@@ -1,6 +1,11 @@
 package DataBase;
 
+import javafx.scene.control.Alert;
 import javafx.scene.control.CheckBox;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.sql.*;
 import java.util.Random;
 
@@ -30,16 +35,24 @@ public class DBConnection {
     public boolean openConnection() {
 
         System.out.print("Trying to connect... ");
-
+        String url = null , username = null , password = null;
+        BufferedReader br;
+        String fileName = "src/DataBase/dbconnectionconfig.txt";
+        try {
+            br = new BufferedReader(new FileReader(fileName));
+            //the parameters for connection to DB.
+            url = br.readLine().replace("url: ","");
+            username = br.readLine().replace("username: ","");
+            password = br.readLine().replace("password: ","");
+        } catch (Exception e) {
+            System.out.println("couldn't read from configuration file - " + e.getMessage());
+        }
         // creating the connection. Parameters should be taken from config file.
         String host = "localhost";
         String port = "3306";
-        String schema = "mm";
-        String user = "root"; //username
-        String password = "Rkehat19"; //password to mysql
 
         try {
-            conn = DriverManager.getConnection("jdbc:mysql://" + host + ":" + port + "/" + schema, user, password);
+            conn = DriverManager.getConnection("jdbc:mysql://" + host + ":" + port + "/" + url, username, password);
         } catch (SQLException e) {
             System.out.println("Unable to connect - " + e.getMessage());
             conn = null;
